2d6955b2ff35f91cf5412ae00e33cabb8a5b159c
Monababy123 <m.sarker12@gmail.com>
2024-12-12 10:03:03 -0500

    added nose arguments

--- service/routes.py
+++ service/routes.py
@@ -143,8 +143,28 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+  
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+        app.logger.info("Account with id %s deleted.", account_id)
+    else:
+        app.logger.warning("Account with id %s not found. Nothing to delete.", account_id)
+
+    return "", status.HTTP_204_NO_CONTENT
 
 
+    
+
+    
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
