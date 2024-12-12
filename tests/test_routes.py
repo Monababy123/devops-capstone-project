ea4d2ddfd70b268a66ee6cafbdba4719e1576f8f
Monababy123 <m.sarker12@gmail.com>
2024-12-12 09:36:03 -0500

    added nose arguments

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -154,5 +154,23 @@ class TestAccountService(TestCase):
         resp = self.client.get(f"{BASE_URL}/{non_existent_id}")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
     
+    def test_get_account_not_found(self):
+        """It should return 404 NOT FOUND when the account does not exist"""
+        non_existent_id = 99999  # Use an ID that is unlikely to exist
+        resp = self.client.get(f"{BASE_URL}/{non_existent_id}")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
