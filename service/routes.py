c188e20eed54446649f38f2850bfe495fd5ae528
Monababy123 <m.sarker12@gmail.com>
2024-12-13 22:20:54 -0500

    Fix whitespace and blank line issues in routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,6 +1,5 @@
 """
 Account Service
-
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
@@ -8,8 +7,6 @@ from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
-
-
 ############################################################
 # Health Endpoint
 ############################################################
@@ -17,8 +14,6 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
-
-
 ######################################################################
 # GET INDEX
 ######################################################################
@@ -33,8 +28,6 @@ def index():
         ),
         status.HTTP_200_OK,
     )
-
-
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
@@ -56,17 +49,10 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
-
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 # ... place you code here to LIST accounts ...
-
-    ######################################################################
-    # LIST ALL ACCOUNTS
-    ######################################################################
-    
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """Returns a List all Accounts"""
@@ -77,17 +63,11 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
-
-
-
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
 # ... place you code here to READ an account ...    ######################################################################
-   
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -106,15 +86,10 @@ def read_account(account_id):
     
     # Serialize the account and return it with HTTP_200_OK
     return jsonify(account.serialize()), status.HTTP_200_OK
-
-
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 # ... place you code here to UPDATE an account ...
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -136,14 +111,10 @@ def update_accounts(account_id):
 
     # Return the updated account serialized as JSON with a 200 OK status
     return jsonify(account.serialize()), status.HTTP_200_OK
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 # ... place you code here to DELETE an account ...
-  
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -159,17 +130,10 @@ def delete_accounts(account_id):
     else:
         app.logger.warning("Account with id %s not found. Nothing to delete.", account_id)
 
-    return "", status.HTTP_204_NO_CONTENT
-
-
-    
-
-    
+    return "", status.HTTP_204_NO_CONTENT 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
