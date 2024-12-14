0bb20ed17fa886ec3e0c9d4e55597e2756003643
Monababy123 <m.sarker12@gmail.com>
2024-12-13 23:15:36 -0500

    Fix whitespace and blank line issues in routes.py

--- service/routes.py
+++ service/routes.py
@@ -35,7 +35,8 @@ def index():
 def create_accounts():
     """
     Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    This endpoint will create an Account based on the data in the body that \
+    is posted.
     """
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
@@ -44,7 +45,9 @@ def create_accounts():
     account.create()
     message = account.serialize()
     # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
+    location_url = url_for(
+    "get_accounts", account_id=account.id, _external=True
+)
     location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
@@ -65,9 +68,9 @@ def list_accounts():
     return jsonify(account_list), status.HTTP_200_OK
 ######################################################################
 # READ AN ACCOUNT
+# ... place you code here to READ an account ...    
 ######################################################################
-
-# ... place you code here to READ an account ...    ######################################################################
+# ... place you code here to READ an account ...   
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -76,19 +79,15 @@ def read_account(account_id):
     If the account is not found, it aborts with a 404 Not Found.
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
     # Attempt to find the account by ID
     account = Account.find(account_id)
-    
-    # Abort with 404 if the account doesn't exist
+    # Abort with 404 if the account
+    # doesn't exist
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id {account_id} not found.")
-    
     # Serialize the account and return it with HTTP_200_OK
     return jsonify(account.serialize()), status.HTTP_200_OK
-######################################################################
 # UPDATE AN EXISTING ACCOUNT
-######################################################################
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -97,18 +96,14 @@ def update_accounts(account_id):
     This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
-
     # Find the account by ID
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     # Deserialize the request JSON into the account object
     account.deserialize(request.get_json())
-    
     # Update the account in the database
     account.update()
-
     # Return the updated account serialized as JSON with a 200 OK status
     return jsonify(account.serialize()), status.HTTP_200_OK
 ######################################################################
@@ -122,14 +117,12 @@ def delete_accounts(account_id):
     This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if account:
         account.delete()
         app.logger.info("Account with id %s deleted.", account_id)
     else:
-        app.logger.warning("Account with id %s not found. Nothing to delete.", account_id)
-
+      app.logger.warning("Account with id %s not found. Nothing to delete.", account_id)
     return "", status.HTTP_204_NO_CONTENT 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
